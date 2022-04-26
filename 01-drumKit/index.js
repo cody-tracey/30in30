d661786d3996c845862c6b46307e097e2b3da289
Weasel <cody.a.tracey@gmail.com>
2022-04-25 22:30:00 -0500

    removed a slioght bit of unecessary code, cleaned up some

--- 01-drumKit/index.js
+++ 01-drumKit/index.js
@@ -1,24 +1,22 @@
 "use strict"
-const elems = [65, 68, 70, 71, 72, 74, 75, 76, 83]
+const elems = [65, 68, 70, 71, 72, 74, 75, 76, 83];
 const down = (e) => {
     if (elems.includes(e.keyCode)) {
         let l = document.body.querySelectorAll(`[data-key="${e.keyCode}"]`)
 
-        if (!l[0].className.includes('playing sound')) l[0].className += ' playing sound';
+        if (!l[0].className.includes('playing')) l[0].className += ' playing';
        l[1].play();
     }
-
-}
+};
 const up = (e) => {
     if (elems.includes(e.keyCode)) {
         let l = document.body.querySelectorAll(`[data-key="${e.keyCode}"]`)
 
         l[0].className = 'key'
         l[1].pause();
- 
+     }
+};
 
-    }
-}
 document.addEventListener('keydown', e => down(e))
 
 document.addEventListener('keyup', e => up(e))
