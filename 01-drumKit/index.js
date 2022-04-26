ab996062289a7a3750d10d8fc20597b8dc61b86b
Weasel <cody.a.tracey@gmail.com>
2022-04-25 22:17:56 -0500

    have in my key down and up functions changing the css on the keys

--- 01-drumKit/index.js
+++ 01-drumKit/index.js
@@ -1,1 +1,25 @@
 "use strict"
+const elems = [65, 68, 70, 71, 72, 74, 75, 76, 83]
+const down = (e) => {
+    if (elems.includes(e.keyCode)) {
+        let l = document.body.querySelectorAll(`[data-key="${e.keyCode}"]`)
+
+        if (!l[0].className.includes('playing sound')) l[0].className += ' playing sound';
+
+    }
+
+
+
+
+}
+const up = (e) => {
+    if (elems.includes(e.keyCode)) {
+        let l = document.body.querySelectorAll(`[data-key="${e.keyCode}"]`)
+
+        l[0].className = 'key'
+
+    }
+}
+document.addEventListener('keydown', e => down(e))
+
+document.addEventListener('keyup', e => up(e))
