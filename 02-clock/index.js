fc297197ab28502d70a202d20a0840a5f04a0300
Weasel <cody.a.tracey@gmail.com>
2022-04-27 19:37:47 -0500

    corrected the hour hand from being on a 24 hr cycle to a 12

--- 02-clock/index.js
+++ 02-clock/index.js
@@ -8,7 +8,7 @@ const setDate = () => {
     const now = new Date();
     const sec = ((now.getSeconds() / 60) * 360 + 90) % 360;
     const min = ((now.getMinutes() / 60) * 360 + 90) % 360;
-    const hr = ((now.getHours() / 24) * 360 + 90) % 360;
+    const hr = ((now.getHours() / 12) * 360 + 90) % 360;
 
     s.style.transform = `rotate(${sec}deg)`
     m.style.transform = `rotate(${min}deg)`
