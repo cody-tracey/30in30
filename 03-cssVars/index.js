2975489756a85992c160d7f1fb7ee46ede525e81
Weasel <cody.a.tracey@gmail.com>
2022-04-27 19:58:30 -0500

    added in logic to grab the elements and their values

---
+++ 03-cssVars/index.js
@@ -0,0 +1,15 @@
+let s = document.getElementById('spacing');
+let bl = document.getElementById('blur');
+let ba = document.getElementById('base');
+
+s.oninput = (e) => {
+    console.log(e.target.value)
+}
+
+bl.oninput = (e) => {
+    console.log(e.target.value)
+}
+
+ba.oninput = (e) => {
+    console.log(e.target.value)
+}
