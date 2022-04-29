a08ce107da432840a70031deea433bddc681bb1f
Weasel <cody.a.tracey@gmail.com>
2022-04-28 22:09:18 -0500

    found an error in 2 functions due to mistype that caused the results to be off, and solved 6

--- 04-arrayCardio/index.js
+++ 04-arrayCardio/index.js
@@ -31,59 +31,67 @@ const people = [
 // 1. Filter the list of inventors for those who were born in the 1500's
 
 let fifteen = inventors.filter(i => i.year > 1499 && i.year < 1600);
-console.log(1,fifteen);
+console.log(1, fifteen);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
 let names = inventors.map(i => `${i.first} ${i.last}`);
-console.log(2,names);
+console.log(2, names);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
-let birthdate = inventors.sort((a, b) => a.year - b.year)
-console.log(3,birthdate);
+let birthdate = inventors.sort((a, b) => {
+    let aLived = a.passed - a.year;
+    let bLived = b.passed - b.year;
+    return bLived - aLived
+})
+console.log(3, birthdate);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
 let lived = inventors.reduce((total, i) => {
     let y = i.passed - i.year;
-   return total += y;
+    return total += y;
 }, 0);
-console.log(4,lived);
+console.log(4, lived);
 
-// 5. Sort the inventors by years lived ***
+// 5. Sort the inventors by years lived 
 
 let livedSorted = inventors.sort((a, b) => {
     let aLived = a.passed - a.year;
-    let bLived = b.abssed - b.year;
-    return aLived - bLived;
+    let bLived = b.passed - b.year;
+    return bLived - aLived
 });
-console.log(5,livedSorted);
+console.log(5, livedSorted);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
 
+// const category = document.querySelector('.mw-category');
+// const links = Array.from(category.querySelectorAll('a'));
+// const de = links.map(i => i.textContent).filter(i => i.includes('de'));
+         
 // 7. sort Exercise
 // Sort the people alphabetically by last name
-let lastSorted = people.map(i => i.split(',')).sort((a,b) => {
-    if(a[0] > b[0]) return 1
-    else if(a[0] < b[0]) return -1
+let lastSorted = people.map(i => i.split(',')).sort((a, b) => {
+    if (a[0] > b[0]) return 1
+    else if (a[0] < b[0]) return -1
     else return 0
 }).map(i => i.join(','))
 
-console.log(7,lastSorted)
+console.log(7, lastSorted)
 
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck'];
 
-let instances = data.reduce((total,i) => {
-    if(total[i]) total[i]++
+let instances = data.reduce((total, i) => {
+    if (total[i]) total[i]++
     else total[i] = 1;
     return total;
-},{});
+}, {});
 
 console.log(instances)
 
