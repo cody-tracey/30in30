6ce7a325059596a6c33267dc218a587964320493
Weasel <cody.a.tracey@gmail.com>
2022-04-30 18:24:06 -0500

    added in the fetch to get the data

--- 06-typeAhead/logic.js
+++ 06-typeAhead/logic.js
@@ -1,1 +1,12 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const locations = [];
+
+let places = fetch(endpoint)
+    .then(res => res.json())
+    .then(data => data.forEach(i => {
+        locations.push(i)
+    }))
+    .catch(err => console.error(err));
+
+console.log(locations)
