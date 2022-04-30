5d317afc8d83f5f2733cdfb05294bac6c5a30f9b
Weasel <cody.a.tracey@gmail.com>
2022-04-30 18:58:22 -0500

    added in display function

--- 06-typeAhead/logic.js
+++ 06-typeAhead/logic.js
@@ -3,19 +3,28 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 
 const cities = [];
 fetch(endpoint)
-  .then(res => res.json())
-  .then(data => cities.push(...data));
+    .then(res => res.json())
+    .then(data => cities.push(...data));
 
 const findMatches = (word, cities) => {
-  return cities.filter(i => {
-    const regex = new RegExp(word, 'gi');
-    return i.city.match(regex) || i.state.match(regex)
-  });
+    return cities.filter(i => {
+        const regex = new RegExp(word, 'gi');
+        return i.city.match(regex) || i.state.match(regex)
+    });
 }
 
 function displayMatches() {
-  const matchArray = findMatches(this.value, cities);
-  console.log(matchArray)
+    const matchArray = findMatches(this.value, cities);
+    const html = matchArray.map(i => {
+        return `
+            <li>
+                <span class='name'>${i.city}, ${i.state}</span>
+                <span class='population'>${i.population}</span>
+            </li>
+        `;
+    }).join('');
+
+    suggestions.innerHTML = html;
 }
 
 const searchInput = document.querySelector('.search');
