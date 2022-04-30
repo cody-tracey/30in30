d1ef827b71de78cc3c15320aedb2fdba0160d1ed
Weasel <cody.a.tracey@gmail.com>
2022-04-30 18:55:04 -0500

    teh filtering and displaying is moving along after debugging the this issue

--- 06-typeAhead/logic.js
+++ 06-typeAhead/logic.js
@@ -1,17 +1,26 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-const locations = [];
 
-let places = fetch(endpoint)
-    .then(res => res.json())
-    .then(data => locations.push(...data))
-    .catch(err => console.error(err));
+const cities = [];
+fetch(endpoint)
+  .then(res => res.json())
+  .then(data => cities.push(...data));
 
+const findMatches = (word, cities) => {
+  return cities.filter(i => {
+    const regex = new RegExp(word, 'gi');
+    return i.city.match(regex) || i.state.match(regex)
+  });
+}
 
-const findMatches = (word,locations) => {
-    return locations.filter(i => {
-        const r = new RegExp(word,'gi')
-        return i.city.match(r) || i.state.match(r)
-    });
-};
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  console.log(matchArray)
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
 
