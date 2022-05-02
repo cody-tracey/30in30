62f53152258c33cbef683488e46ca00b25abc72c
Weasel <cody.a.tracey@gmail.com>
2022-05-02 17:24:24 -0500

    have it drawing currently

--- 08-funWithHtmlCanvas/logic.js
+++ 08-funWithHtmlCanvas/logic.js
@@ -10,6 +10,33 @@ ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 
 let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
+let x = {
+    last: 0,
+    curr: 0
+};
+let y = {
+    last: 0,
+    curr: 0
+};
+
+
+const draw = e => {
+    if (!isDrawing) return;
+    ctx.beginPath();
+    ctx.moveTo(x.last, y.last);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+
+    [x.last, y.last] = [e.offsetX, e.offsetY];
+};
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e) => {
+    [x.last, y.last] = [e.offsetX, e.offsetY];
+    isDrawing = true
+});
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
+
+
 
