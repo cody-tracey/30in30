bb09ba77c45f97bf78a7f2b5eb85ce784eb2e97e
Cody <cody.a.tracey@gmail.com>
2022-05-05 08:44:40 -0500

    added in the volume function

--- 11-CustomVideoPlayer/scripts.js
+++ 11-CustomVideoPlayer/scripts.js
@@ -1,24 +1,32 @@
-const media = document.querySelector('video');
-const playbttn = document.querySelector('.player__button');
-const vol = document.querySelector('input[name="volume"]');
-const playback = document.querySelector('input[name="playbackRate"]');
-const buttons = document.querySelectorAll('button');
-
-
+const player = document.querySelector('.player'),
+media = player.querySelector('video'),
+vol = player.querySelector('input[name="volume"]'),
+playbttn = player.querySelector('.player__button'),
+toggle = player.querySelector('.toggle'),
+progress = player.querySelector('.progress'),
+buttons = player.querySelectorAll('button'),
+playback = player.querySelector('input[name="playbackRate"]');
 
 
 
 
 const playPause = () => {
-    if(media.paused) media.play();
-    else media.pause();
+    if (media.paused) {
+        toggle.textContent = '❚ ❚';
+        media.play();
+    }
+    else {
+        toggle.textContent = '►';
+        media.pause()
+    };
 }
 
-playbttn.addEventListener('click',playPause);
-vol.addEventListener('click', e => console.log(e.target));
+playbttn.addEventListener('click', playPause);
+vol.addEventListener('click', e => media.volume = e.target.value);
 playback.addEventListener('click', e => console.log('play back'));
 buttons.forEach(i => i.addEventListener('click', (e) => {
     let num = Number(e.target.attributes[0].value);
+    console.log(progress)
 }))
 
 
