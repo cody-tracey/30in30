d072778090aceaee6a83c9cc992d1303d7a6fe91
Cody <cody.a.tracey@gmail.com>
2022-05-05 08:26:24 -0500

    all of the selectors are working

--- 11-CustomVideoPlayer/scripts.js
+++ 11-CustomVideoPlayer/scripts.js
@@ -1,5 +1,11 @@
 const media = document.querySelector('video');
-const playB = document.querySelector('.player__button');
+const playbttn = document.querySelector('.player__button');
+const vol = document.querySelector('input[name="volume"]');
+const playback = document.querySelector('input[name="playbackRate"]');
+const buttons = document.querySelectorAll('button');
+
+
+
 
 
 
@@ -8,5 +14,12 @@ const playPause = () => {
     else media.pause();
 }
 
+playbttn.addEventListener('click',playPause);
+vol.addEventListener('click', e => console.log(e.target));
+playback.addEventListener('click', e => console.log('play back'));
+buttons.forEach(i => i.addEventListener('click', (e) => {
+    let num = Number(e.target.attributes[0].value);
+}))
 
-playB.addEventListener('click', playPause)
+
+
