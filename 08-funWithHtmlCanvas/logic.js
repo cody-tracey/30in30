4c2470f4bfd0359c2fe38102c9ff13ababf653be
Weasel <cody.a.tracey@gmail.com>
2022-05-04 20:00:14 -0500

    added in the line width and hue parts

--- 08-funWithHtmlCanvas/logic.js
+++ 08-funWithHtmlCanvas/logic.js
@@ -12,22 +12,28 @@ ctx.lineCap = 'round';
 let isDrawing = false;
 let x = {
     last: 0,
-    curr: 0
 };
 let y = {
     last: 0,
-    curr: 0
 };
+
+let hue = 0;
+let direction = true;
 
 
 const draw = e => {
     if (!isDrawing) return;
+    ctx.strokeStyle =`hsl(${hue},100%,50%)`;
     ctx.beginPath();
     ctx.moveTo(x.last, y.last);
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
 
     [x.last, y.last] = [e.offsetX, e.offsetY];
+    hue++;
+
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {direction = !direction};
+    direction ? ctx.lineWidth++ :ctx.lineWidth-- ;
 };
 
 canvas.addEventListener('mousemove', draw);
