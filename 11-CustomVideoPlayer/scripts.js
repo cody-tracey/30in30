8be7bf191a9009f09084e44aa6562ef49bc606fd
Cody <cody.a.tracey@gmail.com>
2022-05-05 09:28:16 -0500

    added in the final functions, and then refactored based on a tutorial to be better coded

--- 11-CustomVideoPlayer/scripts.js
+++ 11-CustomVideoPlayer/scripts.js
@@ -1,15 +1,12 @@
+(() => {
 const player = document.querySelector('.player'),
     media = player.querySelector('video'),
-    vol = player.querySelector('input[name="volume"]'),
     playbttn = player.querySelector('.player__button'),
     toggle = player.querySelector('.toggle'),
     progress = player.querySelector('.progress'),
+    progressBar = player.querySelector('.progress__filled'),
     buttons = player.querySelectorAll('button'),
-    playback = player.querySelector('input[name="playbackRate"]'),
-    selectors = player.querySelectorAll('.player__slider')
-
-
-
+    selectors = player.querySelectorAll('.player__slider');
 
 const playPause = () => {
     if (media.paused) {
@@ -20,16 +17,46 @@ const playPause = () => {
         toggle.textContent = '►';
         media.pause()
     };
-}
+},
+    handleProgress = () =>
+        progressBar.style.flexBasis = `${media.currentTime / media.duration * 100}%`,
+    scrub = (e) => media.currentTime = ((e.offsetX / progress.offsetWidth) * media.duration),
+    progressMoved = e => mousedown && scrub(e),
+    progressUp = e => mousedown = false,
+    progressDown = e => mousedown = true,
+    bClick = b => media.currentTime += parseFloat(b.dataset.skip),
+    updateRange = (range, e) => media[range.name] = range.value;
+let mousedown = false;
 
-playbttn.addEventListener('click', playPause);
-selectors.forEach(i => addEventListener('click', (e) => {
-    i.addEventListener('change', () => media[i.name] = i.value)
-    i.addEventListener('mousemove', () => media[i.name] = i.value)
-}));
-buttons.forEach(i => i.addEventListener('click', () => {
-    media.currentTime += parseFloat(i.dataset.skip)
-}))
+
+const events = [
+    { event: 'click', handler: playPause, target: playbttn },
+    { event: 'timeupdate', handler: handleProgress, target: media },
+    { event: 'click', handler: playPause, target: media },
+    { event: 'click', handler: scrub, target: progress },
+    { event: "mousedown", handler: progressDown, target: progress },
+    { event: "mousemove", handler: progressMoved, target: progress },
+    { event: "mouseup", handler: progressUp, target: progress },
+    { event: "click", handler: bClick, target: buttons },
+    { event: ["change", "mousemove"], handler: updateRange, target: selectors }
+];
+
+
+events.forEach(
+    ({ event: e, handler: h, target: t }) =>
+      (t instanceof NodeList
+        ? t.forEach((el, i) => {
+            el.addEventListener(
+              typeof e === "string" ? e : e[i],
+              typeof h === "function" ? h.bind(null, el) : h[i].bind(null, el)
+            );
+          })
+        : t.addEventListener(e, h))
+  );
+})();
+
+
+
 
 
 
