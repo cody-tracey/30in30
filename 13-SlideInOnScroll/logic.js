4e8c3626e44fb5036ee398c800b4ab2a76c155e4
Weasel <cody.a.tracey@gmail.com>
2022-05-16 21:12:32 -0500

    reformatted starter function for es6

--- 13-SlideInOnScroll/logic.js
+++ 13-SlideInOnScroll/logic.js
@@ -1,14 +1,26 @@
-function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+(() => {
+    const sliderImg = document.querySelectorAll('.slide-in');
+
+
+
+    const debounce = (func, wait = 20, immediate = true) => {
+        let timeout;
+
+        return (...args) => {
+            const later = () => {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            
+            let callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
     };
-  }
+
+
+})();
+
+
+
