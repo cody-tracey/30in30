f67b8bf0f48d00a9bf5c3ca2ab32613b7ad20774
Weasel <cody.a.tracey@gmail.com>
2022-05-18 21:30:33 -0500

    added in my event listener and start of my function

--- 16-mouseMoveShadow/logic.js
+++ 16-mouseMoveShadow/logic.js
@@ -1,1 +1,13 @@
 'use strict'
+const h1 = document.querySelector('.hero');
+const text = document.querySelector('h1')
+
+const shadow = e => {
+    const { offsetWidth: width, offsetHeight, height } = h1;
+    let { offsetX: x, offsetY: y } = e;
+
+    
+
+}
+
+h1.addEventListener('mouseover', shadow)
