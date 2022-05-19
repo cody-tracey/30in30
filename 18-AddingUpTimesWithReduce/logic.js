3993f1e445a75431c26ae1bc5fea5e7b32d5b492
Cody <cody.a.tracey@gmail.com>
2022-05-19 08:26:47 -0500

    got my starting files set up

---
+++ 18-AddingUpTimesWithReduce/logic.js
@@ -0,0 +1,4 @@
+/*
+The HTML document contains an unordered list with multiple list items, each with a data-time which reflect a time in minutes and seconds. Our goal is to take all of these times and calculate the total in hours, minutes, and seconds.
+*/
+
