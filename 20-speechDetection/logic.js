e07136fbfcb5e6b9dbe92c2f6712ee24a1aca72a
Cody <cody.a.tracey@gmail.com>
2022-05-19 09:29:20 -0500

    solved speech recognition, all hail the power of docs and youtube tutorials

--- 20-speechDetection/logic.js
+++ 20-speechDetection/logic.js
@@ -1,1 +1,26 @@
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+(() => {
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    const recognition = new window.SpeechRecognition();
+    recognition.interimResults = true;
+
+    const words = document.querySelector('.words');
+    let newPElemen = document.createElement('p');
+    words.appendChild(newPElemen);
+
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+        .map(i => i[0])
+        .map(({transcript}) => transcript)
+        .join('');
+
+        newPElemen.textContent = transcript;
+        if(e.results[0].isFinal){
+            newPElemen = document.createElement('p');
+            words.appendChild(newPElemen);
+        };
+    });
+
+    recognition.addEventListener('end',recognition.start)
+    recognition.start();
+});
