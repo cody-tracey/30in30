3842295da135672b42f82258b68408f166f0f5f7
Cody <cody.a.tracey@gmail.com>
2022-05-19 08:52:57 -0500

    added in the logic with a seriesof array methods

--- 18-AddingUpTimesWithReduce/logic.js
+++ 18-AddingUpTimesWithReduce/logic.js
@@ -2,3 +2,22 @@
 The HTML document contains an unordered list with multiple list items, each with a data-time which reflect a time in minutes and seconds. Our goal is to take all of these times and calculate the total in hours, minutes, and seconds.
 */
 
+
+let timesArr = Array.from(document.querySelectorAll('[data-time]'))
+    .map(i => i.getAttribute('data-time').split(':'))
+    .map(i => [parseInt(i[0]), parseInt(i[1])]);
+
+let res = timesArr.reduce((i, total) => {
+    total[0] += i[0];
+    total[1] += i[1];
+    return total;
+}, [0, 0])
+
+
+let secsRem = res[1] % 60;
+let totalMin = ((res[1] - secsRem) / 60) + res[0];
+let minRem = (totalMin) % 60;
+let hours = (totalMin - minRem) / 60;
+
+
+console.log(`Total video time: ${hours} hours, ${minRem} minutes, and ${secsRem} seconds`);
